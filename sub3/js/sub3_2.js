3f74a4e92d592a01f67ad3f46001cfebf38bdae1
parkgee06 <parkgee06@gmail.com>
2024-09-05 22:56:09 +0900

    html 및 css 수정 / 터치스와이퍼 기능 추가

---
+++ sub3/js/sub3_2.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    //centeredSlides: true,
+    spaceBetween: 10,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
